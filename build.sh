5d9329129e47842bd811a29038447969695bbbcb
zekro <riho@live.de>
2018-05-31 15:42:51 +0200

    updated build file

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd $(dirname $0)
 export GOPATH=$PWD
 go get github.com/bwmarrin/discordgo
-go build -o bin/test src/github.com/zekroTutorials/tutorialGoBot/main.go
+go build -o bin/gobot src/github.com/zekroTutorials/tutorialGoBot/main.go
